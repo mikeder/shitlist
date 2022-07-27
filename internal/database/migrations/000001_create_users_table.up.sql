4cf4099b7e3e7315be350255675d008a396cbbba
mikeder <mikedernet@gmail.com>
2022-07-26 20:21:55 -0400

    comment on uuid pk

--- internal/database/migrations/000001_create_users_table.up.sql
+++ internal/database/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,7 @@
 CREATE EXTENSION "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS users(
+   -- https://www.starkandwayne.com/blog/uuid-primary-keys-in-postgresql/
    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
    user_name VARCHAR (50) UNIQUE NOT NULL,
    user_email VARCHAR (300) UNIQUE NOT NULL
